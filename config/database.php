977ea0d2e6bff0c7cbb0950a8023acc6e20263ed
skrevolve <sukyu0919@naver.com>
2020-11-20 10:38:55 +0900

    jetstream upload

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'larevel8'),
+            'database' => env('DB_DATABASE', 'laravel8'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', '478359SKlim!'),
             'unix_socket' => env('DB_SOCKET', ''),
